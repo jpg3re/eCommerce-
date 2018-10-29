b29723869a615fcc666fa362e3be8986445adc1d
Jack Girerd <jpg3re@virginia.edu>
2018-10-29 18:31:08 -0400

    removed ..\

--- sendEmail.php
+++ sendEmail.php
@@ -2,17 +2,14 @@
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
- require '..\vendor\autoload.php';
+ require 'vendor\autoload.php';
 // require 'C:\xampp\PHPMailer\src\Exception.php';
 // require 'C:\xampp\PHPMailer\src\PHPMailer.php';
 //require 'C:\xampp\PHPMailer\src\SMTP.php';
 $mail = new PHPMailer(true);                              // Passing `true` enables exceptions
 try {
 
-    //Recipients
-    // $mail->setFrom('from@example.com', 'Mailer');
-    // $mail->addAddress('joe@example.net', 'Joe User');     // Add a recipient
-    // $mail->addAddress('ellen@example.com');               // Name is optional
+    
 
  $mail->setFrom($_POST['email'], $_POST['name']);
  $mail->addAddress('custombucha@gmail.com');
