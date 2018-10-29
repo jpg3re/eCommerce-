db061c07cc30d80ec6c2cbb53f450e43daad627f
Jack Girerd <jpg3re@virginia.edu>
2018-10-29 19:40:18 -0400

    1

--- sendEmail.php
+++ sendEmail.php
@@ -12,7 +12,7 @@ $mail->Port = 587;
 $mail->Username   = "custombucha@gmail.com"; // SMTP account username
 $mail->Password   = "Custombucha19";        // SMTP account password
 $mail->SMTPDebug=4;
-$mail->SMTPSecure='ssl';
+//$mail->SMTPSecure='ssl';
  $mail->setFrom($_POST['email'], $_POST['name']);
  $mail->addAddress('custombucha@gmail.com');
  $mail->Subject  = $_POST['subject'];
