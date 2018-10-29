9bf803699b2f6834b7aaa7caad04e5d4c0624b5b
Jack Girerd <jpg3re@virginia.edu>
2018-10-29 19:34:46 -0400

    smtpsecure

--- sendEmail.php
+++ sendEmail.php
@@ -11,7 +11,8 @@ $mail->Host ="smtp.gmail.com";
 $mail->Port = 587;  
 $mail->Username   = "custombucha@gmail.com"; // SMTP account username
 $mail->Password   = "Custombucha19";        // SMTP account password
-$mail->SMTPDebug=4;
+//$mail->SMTPDebug=4;
+$mail->SMTPSecure='ssl';
  $mail->setFrom($_POST['email'], $_POST['name']);
  $mail->addAddress('custombucha@gmail.com');
  $mail->Subject  = $_POST['subject'];
