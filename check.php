9d40b3a5de98bb22660ae288b39bb2e590648ec0
elr8cu <elr8cu@virginia.edu>
2018-11-16 15:04:17 -0500

    Fixed password for log-in

--- check.php
+++ check.php
@@ -3,10 +3,10 @@
 $db_connection = pg_connect("host=ec2-184-72-234-230.compute-1.amazonaws.com port=5432 dbname=d9n13lvg10h48 user=cmjanmfuwvlbwe password=
 97d83c70e48411627d817565d0b1d3f2d592e6df32448158238e69eae50a61aa");
 
-$password= $_POST['password'];
-$hashed_password = password_hash($password, PASSWORD_DEFAULT);
+$this_password= $_POST['password'];
+$hashed_password = password_hash($this_password, PASSWORD_DEFAULT);
 $email=$_POST['email'];
-$check=pg_query($db_connection,"SELECT hashed_password FROM siteusers WHERE email='$email'");
+$check=pg_query($db_connection,"SELECT password FROM siteusers WHERE email='$email'");
 if($check == $hashed_password){
 	header('Location: member-home.php');
 }
