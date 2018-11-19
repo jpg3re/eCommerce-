cf22c94371d24851528b3dd068836fc12d2bb76c
Jillian DeWoody <jilliandewoody@Jillians-MacBook-Pro.local>
2018-11-19 15:32:49 -0500

    my profile 8

--- edit.php
+++ edit.php
@@ -7,6 +7,11 @@ require 'vendor/autoload.php';
 
 $db_connection = pg_connect("host=ec2-184-72-234-230.compute-1.amazonaws.com port=5432 dbname=d9n13lvg10h48 user=cmjanmfuwvlbwe password=
 97d83c70e48411627d817565d0b1d3f2d592e6df32448158238e69eae50a61aa");
-	$query = "UPDATE siteusers SET address = $_POST[address], city = $_POST[city], state = $_POST[state], zip_code = $_POST[zip_code] WHERE email = $_SESSION["email"]";
+  $a = $_POST['address'];
+  $c = $_POST['city'];
+  $s = $_POST['state'];
+  $z = $_POST['zip_code'];
+  $e = $_SESSION['email'];
+	$query = "UPDATE siteusers SET address = '$a', city = '$c', state = '$s', zip_code = '$z' WHERE email = '$e'";
   $result = pg_query($query);
 ?>
